782fff2012a12c850c0d5f93e4bcd5d7e610508a
sriharna <sriharishnagarajancse@gmail.com>
2019-12-15 13:37:01 +0530

    react-phonebook-initial-commit

---
+++ part2/src/components/App.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react'
+
+const App = () => {
+    const [names, setNames] = useState('');
+    const [persons, setPersons] = useState([]);
+   const addNumbers = (event) =>{
+       event.preventDefault();
+       console.log('button clicked',event.target);
+       setPersons(persons.concat({name: names}));
+       setNames('');
+   }
+   const changeEvent = (event) =>{
+      console.log(event.target.value);
+      setNames(event.target.value);
+   }
+   return(
+       <div>
+           <h2>PhoneBook</h2>
+           <form onSubmit = {addNumbers}>
+               <div> name : <input value={names} onChange= {changeEvent}/></div>
+               <div> <button type="submit">add</button></div>
+           </form>
+           <div> Names entered : 
+               {/* {persons.map(person => <ul><li>{person.name}</li></ul>)} */}
+               {persons.map(person => <div>{person.name}</div>)}
+               </div>   
+       </div>
+   )
+}
+
+export default App
